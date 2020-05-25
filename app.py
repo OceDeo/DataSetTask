065ce155416c21ec5d05141d15765fc65c9a12af
Lukasz Mildner <lukasz1330@gmail.com>
2020-05-25 17:29:35 +0100

    Comment added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import operator
 homeDir = os.environ['HOME'] 
 
 
-def bottom_five_rent():
+def bottom_five_rent(): # show 5 companies with the lowest rent 
     with open(homeDir + "/Desktop/Python/DataSetTask/dataset.csv", "r") as dataset:
         sorted_list = sorted(csv.reader(dataset.readlines(), delimiter=','), key=operator.itemgetter(10), reverse=True)
     for line in sorted_list[1:6]:
