f4e796fb18f1bac1e3f367cc2269cc142076e261
Lukasz Mildner <lukasz1330@gmail.com>
2020-05-25 17:54:15 +0100

    Added lease_year_25 function

--- app.py
+++ app.py
@@ -11,3 +11,15 @@ def bottom_five_rent(): # show 5 companies with the lowest rent
         print(line)
 
 
+def lease_year_25():
+    lease_year_25_list = []
+    total_rent = 0
+    with open(homeDir + "/Desktop/Python/DataSetTask/dataset.csv", "r") as dataset:
+        for line in csv.reader(dataset.readlines(), delimiter=','):
+            if line[9] == '25':
+                total_rent = total_rent + float(line[10])
+                lease_year_25_list.append(line)
+        print(lease_year_25_list)
+        print(total_rent)
+
+lease_year_25()
